0b3b56fde9cd34c143c9f43c89f0dab5b9f6a090
Taewoo Kim <rxd0414@gmail.com>
2020-10-05 11:28:43 +0900

    Template Engine

---
+++ 01.Template_Engine/04.forever.js
@@ -0,0 +1,21 @@
+require('http').createServer(function (request, response){
+    if (request.url === '/' || request.url === '/favicon.ico') {
+        let html =
+        `<!DOCTYPE html>
+        <html lang="ko">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Forever</title>
+        </head>
+        <body>
+            <h1>Forever</h1>
+        </body>
+        </html>`;
+        response.end(html);
+    } else {
+        error.error.error();
+    }
+}).listen(3000,function(){
+    console.log('Server running at http://127.0.0.1:3000');
+})
